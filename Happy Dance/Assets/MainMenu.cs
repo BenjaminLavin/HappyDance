2eff9b6f3dcf8e862adb926116d1e7df3027c9d6
nbeck <nbeck@ualberta.ca>
2018-01-31 19:09:42 -0700

    added main menu assets to version control

---
+++ Happy Dance/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame ()
+    {
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("Play button pressed");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit Game");
+        Application.Quit();
+    }
+}   
