f8df16bd24fdd9d4b38b63c06a627b1f323bf121
nurnbeck <nbeck@ualberta.ca>
2018-02-16 10:14:34 -0700

    added main export function for CSV management

---
+++ Happy Dance/Assets/CSVManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CsvFile;
+
+public class CSVManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//create a csv
+	}
+	
+	void Submit () {
+		//get applicable variables
+
+		//update csv
+		using (var csvFile = new CsvFile<Client>("clients.csv"))
+		{
+    	for (int i = 0 ; i < 1000000 ; i++)
+    	{
+        	var client = new Client() { id = i, name = "Client " + i };
+        	csvFile.Append(user);
+    	}
+		}    
+
+	}
+}
