5444aa90b1d590b5beae9237966a69ba770c3fc2
justinwidney <jwidney@ualberta.ca>
2018-02-26 10:02:57 -0700

    added test to file

--- Happy Dance/tests/csvTest.cs
+++ Happy Dance/tests/csvTest.cs
@@ -2,15 +2,35 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using Microsoft.VisualBasic.FileIO;
 
 
 // Testing out Nolan CSV command for starting the game
 public class NewPlayModeTest {
 
+	var path = @"C:\YourFile.csv";
+	
 	[Test]
 	public void NewPlayModeTestSimplePasses() {
-        MainMenu class1 = new MainMenu();
-        class1.PlayGame();
+        MainMenu dummyClass = new MainMenu();
+        dummyClass.PlayGame(); // create csv file
+		
+	// test to see if the file exists
+	using (var parser = new TextFieldParser(path))
+	{
+    	parser.TextFieldType = FieldType.Delimited;
+  	parser.SetDelimiters(",");
+
+    	string[] line;
+   	while (!parser.EndOfData)
+    	{
+        	try {line = parser.ReadFields();}
+        catch (MalformedLineException ex)
+        {
+            // log ex.Message
+        }
+    }
+}
     }
 
 
