b32c96ba4b3a69870c3f1036e51cedb7130265f4
jwidney <jwidney@ualberta.ca>
2018-02-26 09:57:19 -0700

    added first test

---
+++ Happy Dance/tests/csvTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+
+// Testing out Nolan CSV command for starting the game
+public class NewPlayModeTest {
+
+	[Test]
+	public void NewPlayModeTestSimplePasses() {
+        MainMenu class1 = new MainMenu();
+        class1.PlayGame();
+    }
+
+
+}
