b265df3df14c2f3d8f12b0c492e02e9a62e8ae15
blavin <benjaminlavin2109@gmail.com>
2018-04-08 18:54:26 -0600

    Music fade out

--- DynamEnv/Assets/Scripts/TimerManager.cs
+++ DynamEnv/Assets/Scripts/TimerManager.cs
@@ -18,11 +18,25 @@ public class TimerManager : MonoBehaviour {
 
 	public void cameraout(){
 		mainCam.GetComponent<Animator> ().SetTrigger ("cameraout");
+		StartCoroutine (FadeOut (mainCam.GetComponent<AudioSource>(), 7f));
 		Invoke ("fadeout", 7f);
         danceDone = true;
 	}
 
 	void fadeout(){
 		StartCoroutine (GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In,"ExportMenu"));
+	}
+
+	public static IEnumerator FadeOut (AudioSource audioSource, float FadeTime) {
+		float startVolume = audioSource.volume;
+
+		while (audioSource.volume > 0) {
+			audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+			yield return null;
+		}
+
+		audioSource.Stop ();
+		audioSource.volume = startVolume;
 	}
 }
